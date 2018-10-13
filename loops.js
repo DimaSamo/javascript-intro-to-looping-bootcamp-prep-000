9474b44a08e35c1e714d39f934740e2a5b398bf5
Dmitri Samotessov <dimasamo96@gmail.com>
2018-10-13 21:48:20 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop (array){
-  for (var i=0, i<=24, i++){
-    array[i]===`I am ${i-1} strangle loops.`
+  for (var i=1, i<=25, i++){
+    array[i]===`I am ${i} strangle loops.`
   }
   return array
 }
