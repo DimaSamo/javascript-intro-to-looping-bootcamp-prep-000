bba902e49724adec5c75989b5036a8a416f010da
Dmitri Samotessov <dimasamo96@gmail.com>
2018-10-13 21:49:41 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,11 @@
 function forLoop (array){
   for (var i=1, i<=25, i++){
+    if i=1 {
+      array[1]="I am 1 strange loop."
+    }
+    else{
     array[i]===`I am ${i} strangle loops.`
+    }
   }
   return array
 }
